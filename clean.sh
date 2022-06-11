97cac1c70be044d857ba717ec095626b6b097f95
Chris J. Breisch <chris.github@breisch.org>
2022-06-11 19:54:30 -0400

    Delete clean.sh

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for dir in $(ls -R | grep -e 'bin:' ); do
-   path=$(echo -n $dir | sed -e 's/://')
-   rm -rvf $path
-done
-for dir in $(ls -R | grep -e 'obj:' ); do
-   path=$(echo -n $dir | sed -e 's/://')
-   rm -rvf $path
-done
