768d9cfeac952b3c0c334bd0298d9c764599b171
cjbreisch <chris.github@breisch.org>
2026-08-16 13:42:50 -0400

    Cover open-ended LLIST ranges

--- Trs80.Level1Basic.Interpreter.Test/Level2CompatibilityTest.cs
+++ Trs80.Level1Basic.Interpreter.Test/Level2CompatibilityTest.cs
@@ -82,6 +82,24 @@ public class Level2CompatibilityTest
     }
 
     [TestMethod]
+    public void Llist_Open_Ended_Range_Prints_From_The_Starting_Line()
+    {
+        using var controller = new TestController();
+        controller.ExecuteStatements(new List<string> {
+            "10 PRINT \"TEN\"",
+            "20 PRINT \"TWENTY\"",
+            "30 PRINT \"THIRTY\""
+        });
+
+        controller.ExecuteLine("LLIST 20-");
+
+        controller.Host.PrintedDocuments.Should().ContainSingle();
+        controller.Host.PrintedDocuments[0].Should().NotContain(" 10  ");
+        controller.Host.PrintedDocuments[0].Should().Contain(" 20  PRINT \"TWENTY\"");
+        controller.Host.PrintedDocuments[0].Should().Contain(" 30  PRINT \"THIRTY\"");
+    }
+
+    [TestMethod]
     public void Hardware_Statements_Preserve_Graphics_And_Memory_Behavior()
     {
         using var controller = new TestController();
