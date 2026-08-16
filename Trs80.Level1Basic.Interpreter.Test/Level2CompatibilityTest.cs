0c8a540d5160660e2614628eaeb288eed387a31e
cjbreisch <chris.github@breisch.org>
2026-08-15 22:03:17 -0400

    Expand CDBL compatibility corpus

--- Trs80.Level1Basic.Interpreter.Test/Level2CompatibilityTest.cs
+++ Trs80.Level1Basic.Interpreter.Test/Level2CompatibilityTest.cs
@@ -769,4 +769,20 @@ public class Level2CompatibilityTest
         controller.ReadOutputLine().Should().Be(" 1  1 ");
         controller.IsEndOfRun().Should().BeTrue();
     }
+
+    [TestMethod]
+    public void Double_Conversion_Compatibility_Program()
+    {
+        using var controller = new TestController();
+        var program = new List<string> {
+            "10 DEFSNG A",
+            "20 A=1/3",
+            "30 PRINT CDBL(A)"
+        };
+
+        controller.RunProgram(program);
+
+        controller.ReadOutputLine().Should().Be(" 0.3333333 ");
+        controller.IsEndOfRun().Should().BeTrue();
+    }
 }
